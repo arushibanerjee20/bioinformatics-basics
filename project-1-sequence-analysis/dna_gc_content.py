6c3bc614ae175e66a1782f2885c56bf57468fb9b
arushibanerjee20 <arushibanerjee2005@gmail.com>
2026-01-28 20:28:47 +0530

    Update dna_gc_content.py

--- project-1-sequence-analysis/dna_gc_content.py
+++ project-1-sequence-analysis/dna_gc_content.py
@@ -1,8 +1,13 @@
 dna_sequence = input("Enter a DNA sequence: ").upper()
 
+valid_bases = {"A", "T", "G", "C"}
 
-gc_count = dna_sequence.count("G") + dna_sequence.count("C")
-gc_content = (gc_count / len(dna_sequence)) * 100
+if not set(dna_sequence).issubset(valid_bases):
+    print("Invalid DNA sequence. Please use only A, T, G, C.")
+else:
+    gc_count = dna_sequence.count("G") + dna_sequence.count("C")
+    gc_content = (gc_count / len(dna_sequence)) * 100
 
-print("DNA sequence:", dna_sequence)
-print("GC content:", gc_content, "%")
+    print("DNA sequence:", dna_sequence)
+    print("GC content:", gc_content, "%")
+
